83c550cdb8cc255c2c4566644af399253e8439d3
marcelc63 <marcelc6363@gmail.com>
2026-05-11 10:52:51 +0700

    Enhance VibePresto admin functionality with page editor support and bundle management features

---
+++ assets/editor-sidebar.js
@@ -0,0 +1,282 @@
+(function (wp) {
+    var registerPlugin = wp.plugins.registerPlugin;
+    var PluginDocumentSettingPanel = wp.editPost.PluginDocumentSettingPanel;
+    var el = wp.element.createElement;
+    var Fragment = wp.element.Fragment;
+    var useState = wp.element.useState;
+    var useSelect = wp.data.useSelect;
+    var useDispatch = wp.data.useDispatch;
+    var components = wp.components;
+
+    var data = window.VibePrestoEditorSidebar;
+    if (! data) {
+        return;
+    }
+
+    function FileField(props) {
+        return el(
+            components.BaseControl,
+            {
+                label: props.label,
+                help: props.help || undefined,
+            },
+            el('input', {
+                type: 'file',
+                name: props.name,
+                accept: props.accept || undefined,
+                multiple: !!props.multiple,
+            })
+        );
+    }
+
+    function renderAssignmentTab(context) {
+        var strings = context.strings;
+        var bundleOptions = context.bundleOptions;
+        var selectedBundleId = context.selectedBundleId;
+        var onBundleChange = context.onBundleChange;
+        var activeBundle = context.activeBundle;
+        var links = context.links;
+
+        return [
+            el(components.SelectControl, {
+                key: 'bundle-select',
+                label: strings.assignTitle,
+                help: strings.assignHelp,
+                value: String(selectedBundleId),
+                options: bundleOptions,
+                onChange: onBundleChange,
+            }),
+            activeBundle ? el(
+                Fragment,
+                { key: 'active-bundle' },
+                el('p', null, el('strong', null, strings.activeTitle)),
+                el('p', null, activeBundle.title),
+                el('p', null, strings.activeDescription),
+                el('p', null, strings.mode + ': ' + activeBundle.mode),
+                el('p', null, strings.entryHtml + ': ', el('code', null, activeBundle.entry_html)),
+                links.preview ? el('p', null, strings.frontendUrl + ': ', el('a', {
+                    href: links.preview,
+                    target: '_blank',
+                    rel: 'noopener noreferrer'
+                }, strings.openPage)) : null
+            ) : el('p', { key: 'no-bundle' }, strings.noBundleActive),
+            el(
+                'p',
+                { key: 'assignment-actions' },
+                links.preview ? el(components.Button, {
+                    variant: 'secondary',
+                    href: links.preview,
+                    target: '_blank'
+                }, strings.previewPage) : null,
+                links.preview ? ' ' : null,
+                el(components.Button, {
+                    variant: 'link',
+                    href: links.manageBundles
+                }, strings.manageBundles)
+            )
+        ];
+    }
+
+    function renderUploadTab(context) {
+        var strings = context.strings;
+        var links = context.links;
+        var nonces = context.nonces;
+        var postId = context.postId;
+        var uploadMode = context.uploadMode;
+        var setUploadMode = context.setUploadMode;
+        var assignOnUpload = context.assignOnUpload;
+        var setAssignOnUpload = context.setAssignOnUpload;
+
+        return [
+            el('p', { key: 'upload-help' }, strings.uploadHelp),
+            postId < 1 ? el(components.Notice, {
+                key: 'save-first',
+                status: 'info',
+                isDismissible: false,
+            }, strings.saveFirstToUpload) : null,
+            postId > 0 ? el(
+                'form',
+                {
+                    key: 'upload-form',
+                    method: 'post',
+                    action: links.adminPost,
+                    encType: 'multipart/form-data',
+                },
+                el('input', {
+                    type: 'hidden',
+                    name: 'action',
+                    value: 'vibepresto_create_bundle',
+                }),
+                el('input', {
+                    type: 'hidden',
+                    name: 'page_id',
+                    value: String(postId),
+                }),
+                el('input', {
+                    type: 'hidden',
+                    name: '_wpnonce',
+                    value: nonces.createBundle,
+                }),
+                el('input', {
+                    type: 'hidden',
+                    name: 'upload_mode',
+                    value: uploadMode,
+                }),
+                assignOnUpload ? el('input', {
+                    type: 'hidden',
+                    name: 'assign_to_page',
+                    value: '1',
+                }) : null,
+                el(components.TextControl, {
+                    label: strings.displayName,
+                    name: 'display_name',
+                    placeholder: strings.displayNamePlaceholder,
+                }),
+                el(components.RadioControl, {
+                    label: strings.uploadMode,
+                    selected: uploadMode,
+                    options: [
+                        { label: strings.zipBundle, value: 'zip' },
+                        { label: strings.separateFiles, value: 'separate' }
+                    ],
+                    onChange: setUploadMode,
+                }),
+                uploadMode === 'zip' ? el(FileField, {
+                    label: strings.zipBundle,
+                    name: 'bundle_zip',
+                    accept: '.zip',
+                    help: strings.zipDescription,
+                }) : null,
+                uploadMode === 'separate' ? el(Fragment, null,
+                    el(FileField, {
+                        label: strings.htmlFile,
+                        name: 'bundle_html',
+                        accept: '.html,.htm',
+                    }),
+                    el(FileField, {
+                        label: strings.cssFile,
+                        name: 'bundle_css',
+                        accept: '.css',
+                    }),
+                    el(FileField, {
+                        label: strings.jsFile,
+                        name: 'bundle_js',
+                        accept: '.js',
+                    }),
+                    el(FileField, {
+                        label: strings.extraAssets,
+                        name: 'bundle_assets[]',
+                        multiple: true,
+                        help: strings.assetsDescription,
+                    })
+                ) : null,
+                el(components.CheckboxControl, {
+                    label: strings.assignOnUpload,
+                    checked: assignOnUpload,
+                    onChange: setAssignOnUpload,
+                }),
+                el(components.Button, {
+                    variant: 'primary',
+                    type: 'submit',
+                }, strings.uploadButton)
+            ) : null
+        ];
+    }
+
+    function SidebarPanel() {
+        var metaKey = data.metaKey;
+        var bundles = data.bundles || [];
+        var strings = data.strings || {};
+        var notice = data.notice;
+        var links = data.links || {};
+        var nonces = data.nonces || {};
+        var postMeta = useSelect(function (select) {
+            return select('core/editor').getEditedPostAttribute('meta') || {};
+        }, []);
+        var selectedBundleId = Number(postMeta[metaKey] || 0);
+        var activeBundle = bundles.find(function (bundle) {
+            return Number(bundle.id) === selectedBundleId;
+        }) || null;
+        var uploadState = useState('zip');
+        var uploadMode = uploadState[0];
+        var setUploadMode = uploadState[1];
+        var assignState = useState(data.postId > 0);
+        var assignOnUpload = assignState[0];
+        var setAssignOnUpload = assignState[1];
+        var dispatch = useDispatch('core/editor');
+
+        function onBundleChange(value) {
+            var next = {};
+            next[metaKey] = Number(value || 0);
+
+            dispatch.editPost({
+                meta: Object.assign({}, postMeta, next)
+            });
+        }
+
+        var bundleOptions = [{
+            label: strings.normalRendering,
+            value: '0'
+        }].concat(bundles.map(function (bundle) {
+            return {
+                label: bundle.title + ' (' + bundle.mode + ')',
+                value: String(bundle.id)
+            };
+        }));
+
+        return el(
+            PluginDocumentSettingPanel,
+            {
+                name: 'vibepresto-takeover',
+                title: strings.panelTitle,
+            },
+            notice ? el(components.Notice, {
+                status: notice.type === 'success' ? 'success' : 'error',
+                isDismissible: false,
+            }, notice.message) : null,
+            el('p', null, strings.panelHelp),
+            el(components.TabPanel, {
+                className: 'vibepresto-sidebar-tabs',
+                activeClass: 'is-active',
+                tabs: [
+                    {
+                        name: 'assignment',
+                        title: strings.assignmentTab,
+                        className: 'vibepresto-sidebar-tab-assignment',
+                    },
+                    {
+                        name: 'upload',
+                        title: strings.uploadTab,
+                        className: 'vibepresto-sidebar-tab-upload',
+                    }
+                ]
+            }, function (tab) {
+                if (tab.name === 'upload') {
+                    return renderUploadTab({
+                        strings: strings,
+                        links: links,
+                        nonces: nonces,
+                        postId: data.postId,
+                        uploadMode: uploadMode,
+                        setUploadMode: setUploadMode,
+                        assignOnUpload: assignOnUpload,
+                        setAssignOnUpload: setAssignOnUpload,
+                    });
+                }
+
+                return renderAssignmentTab({
+                    strings: strings,
+                    bundleOptions: bundleOptions,
+                    selectedBundleId: selectedBundleId,
+                    onBundleChange: onBundleChange,
+                    activeBundle: activeBundle,
+                    links: links,
+                });
+            })
+        );
+    }
+
+    registerPlugin('vibepresto-editor-sidebar', {
+        render: SidebarPanel,
+    });
+}(window.wp));
--- includes/class-vibepresto-admin.php
+++ includes/class-vibepresto-admin.php
@@ -19,11 +19,33 @@ class Admin
 
     public function register(): void
     {
+        add_action('init', [$this, 'register_page_editor_support']);
+        add_action('init', [$this, 'register_page_assignment_meta']);
         add_action('admin_menu', [$this, 'register_menu']);
         add_action('admin_post_vibepresto_create_bundle', [$this, 'handle_create_bundle']);
         add_action('admin_post_vibepresto_delete_bundle', [$this, 'handle_delete_bundle']);
         add_action('add_meta_boxes_page', [$this, 'register_page_meta_box']);
+        add_action('enqueue_block_editor_assets', [$this, 'enqueue_block_editor_assets']);
         add_action('save_post_page', [$this, 'save_page_assignment']);
+    }
+
+    public function register_page_editor_support(): void
+    {
+        add_post_type_support('page', 'custom-fields');
+    }
+
+    public function register_page_assignment_meta(): void
+    {
+        register_post_meta('page', Bundle_Repository::PAGE_META_KEY, [
+            'type' => 'integer',
+            'single' => true,
+            'default' => 0,
+            'show_in_rest' => true,
+            'sanitize_callback' => 'absint',
+            'auth_callback' => static function (): bool {
+                return current_user_can('manage_options');
+            },
+        ]);
     }
 
     public function register_menu(): void
@@ -105,14 +127,52 @@ class Admin
 
     public function register_page_meta_box(): void
     {
+        if (use_block_editor_for_post_type('page')) {
+            return;
+        }
+
         add_meta_box(
             'vibepresto-page-assignment',
             __('VibePresto Takeover', 'vibepresto'),
             [$this, 'render_page_meta_box'],
             'page',
-            'normal',
-            'high'
+            'side',
+            'default'
         );
+    }
+
+    public function enqueue_block_editor_assets(): void
+    {
+        if (! current_user_can('manage_options')) {
+            return;
+        }
+
+        $screen = get_current_screen();
+        if (! $screen || $screen->base !== 'post' || $screen->post_type !== 'page' || ! method_exists($screen, 'is_block_editor') || ! $screen->is_block_editor()) {
+            return;
+        }
+
+        $post_id = isset($_GET['post']) ? (int) $_GET['post'] : 0;
+        $post = $post_id > 0 ? get_post($post_id) : null;
+        if ($post && (! $post instanceof \WP_Post || $post->post_type !== 'page')) {
+            return;
+        }
+
+        wp_register_script(
+            'vibepresto-editor-sidebar',
+            VIBEPRESTO_PLUGIN_URL . 'assets/editor-sidebar.js',
+            ['wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins'],
+            VIBEPRESTO_VERSION,
+            true
+        );
+
+        wp_add_inline_script(
+            'vibepresto-editor-sidebar',
+            'window.VibePrestoEditorSidebar = ' . wp_json_encode($this->get_editor_sidebar_data($post)) . ';',
+            'before'
+        );
+
+        wp_enqueue_script('vibepresto-editor-sidebar');
     }
 
     public function render_page_meta_box(\WP_Post $post): void
@@ -193,6 +253,63 @@ class Admin
         return [
             'message' => sanitize_text_field(rawurldecode(wp_unslash($_GET['vibepresto_notice']))),
             'type' => sanitize_key($_GET['vibepresto_notice_type']),
+        ];
+    }
+
+    private function get_editor_sidebar_data(?\WP_Post $post): array
+    {
+        $post_id = $post instanceof \WP_Post ? (int) $post->ID : 0;
+        $selected_bundle_id = $post_id > 0 ? $this->bundles->get_assigned_bundle_id($post_id) : 0;
+
+        return [
+            'postId' => $post_id,
+            'postType' => 'page',
+            'metaKey' => Bundle_Repository::PAGE_META_KEY,
+            'selectedBundleId' => $selected_bundle_id,
+            'bundles' => $this->bundles->all(),
+            'notice' => $this->read_notice(),
+            'links' => [
+                'preview' => $post_id > 0 ? get_permalink($post_id) : '',
+                'manageBundles' => admin_url('admin.php?page=vibepresto'),
+                'adminPost' => admin_url('admin-post.php'),
+            ],
+            'nonces' => [
+                'createBundle' => wp_create_nonce('vibepresto_create_bundle'),
+            ],
+            'strings' => [
+                'panelTitle' => __('VibePresto Takeover', 'vibepresto'),
+                'panelHelp' => __('Use VibePresto to replace this page with an uploaded HTML/CSS/JS bundle.', 'vibepresto'),
+                'assignmentTab' => __('Assignment', 'vibepresto'),
+                'uploadTab' => __('Upload', 'vibepresto'),
+                'activeTitle' => __('Active bundle', 'vibepresto'),
+                'activeDescription' => __('Visitors see this takeover bundle instead of the normal WordPress page template.', 'vibepresto'),
+                'mode' => __('Mode', 'vibepresto'),
+                'entryHtml' => __('Entry HTML', 'vibepresto'),
+                'frontendUrl' => __('Frontend URL', 'vibepresto'),
+                'openPage' => __('Open page', 'vibepresto'),
+                'assignTitle' => __('Assigned bundle', 'vibepresto'),
+                'assignHelp' => __('Choose which uploaded bundle should control this page. Save or update the page to apply changes.', 'vibepresto'),
+                'normalRendering' => __('Use normal WordPress rendering', 'vibepresto'),
+                'previewPage' => __('Preview current page', 'vibepresto'),
+                'manageBundles' => __('Manage all bundles', 'vibepresto'),
+                'uploadTitle' => __('Upload new bundle', 'vibepresto'),
+                'uploadHelp' => __('Upload a ZIP bundle or separate HTML/CSS/JS files from the editor sidebar.', 'vibepresto'),
+                'displayName' => __('Display name', 'vibepresto'),
+                'displayNamePlaceholder' => __('Landing page bundle', 'vibepresto'),
+                'uploadMode' => __('Upload mode', 'vibepresto'),
+                'zipBundle' => __('ZIP bundle', 'vibepresto'),
+                'separateFiles' => __('Separate files', 'vibepresto'),
+                'htmlFile' => __('HTML file', 'vibepresto'),
+                'cssFile' => __('CSS file', 'vibepresto'),
+                'jsFile' => __('JS file', 'vibepresto'),
+                'extraAssets' => __('Extra assets', 'vibepresto'),
+                'zipDescription' => __('Best for exported landing pages with nested folders and assets.', 'vibepresto'),
+                'assetsDescription' => __('Optional images, fonts, and other files referenced by your HTML.', 'vibepresto'),
+                'assignOnUpload' => __('Assign the uploaded bundle to this page immediately', 'vibepresto'),
+                'uploadButton' => __('Upload and keep editing', 'vibepresto'),
+                'noBundleActive' => __('No bundle is currently assigned to this page.', 'vibepresto'),
+                'saveFirstToUpload' => __('Save the page first if you want to upload and assign a bundle from the editor sidebar.', 'vibepresto'),
+            ],
         ];
     }
 }
